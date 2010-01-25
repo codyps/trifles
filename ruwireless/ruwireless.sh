657a3537437c0b93a61aab490536a69b979b3c25
John Mesmon <jmesmon@gmail.com>
2010-01-25 04:43:00 -0500

    added ruwireless login script

---
+++ ruwireless/ruwireless.sh
@@ -0,0 +1,235 @@
+#!/bin/sh
+LOGIN_SCRIPT_URL=https://login.ruw.rutgers.edu/login.pl
+
+usage () {
+	echo "usage: 	"
+	echo "	$0 login [username [password]]"
+	echo "	$0 logout"
+	echo "	$0 status"
+	exit 1
+}
+
+read_pass () {
+	if [ $# -ne 2 ]; then
+		echo "read_pass: arg count"
+		return '-1'
+	fi
+
+	trap "stty echo;echo ;exit 1;" INT TERM EXIT	
+
+	stty -echo
+	read -p $1 $2; echo
+	stty echo
+
+	trap - INT TERM EXIT
+
+	return 0
+}
+
+ruw_login () {
+	# Login
+	local ru_user ru_pass
+
+	if [ $# -eq 1 ]; then
+		read -p "Username: " ru_user
+		read_pass "Password:  " ru_pass
+	elif [ $# -eq 2 ]; then
+		ru_user=$2
+		read_pass "Password:  " ru_pass
+	elif [ $# -eq 3 ]; then
+		ru_user=$2
+		ru_pass=$3
+	else
+		echo "error: incorrect parameters to login"
+		usage
+	fi
+
+	local login_page=$(curl -s	\
+		--form-string bs_user="$ru_user"	\
+		--form-string bs_password="$ru_pass"	\
+		-F which_form="reg"	\
+		-F _FORM_SUBMIT="1"	\
+		-F error=""		\
+		-F destination=		\
+		-F "source="		\
+		$LOGIN_SCRIPT_URL	\
+		)
+
+	echo "$login_page"
+
+	echo "not done"
+}
+
+ruw_logout () {
+	echo "not done"
+}
+
+get_ip_and_uid () {
+	#local first_page=$(curl -m 1 -s $LOGIN_SCRIPT_URL)
+	local first_page=$(wget -T 1 -q ${LOGIN_SCRIPT_URL} -O -)
+	if [ -z "$first_page" ]; then
+		return 2
+	fi
+	
+	local source_ip=$(echo "\'$first_page\'" | grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}' )
+	local uid=$(echo "\'$first_page\'" | grep -E -o '' )
+
+	
+}
+
+ruw_status () {
+	local first_page source_ip
+	# If not logged in: contains the login form (with 'source' IP and 'destination' URL)
+	# If logged in		: contains the login form (with 'error' = "You are already logged in")
+	#first_page=$(curl -m 1 -s $LOGIN_SCRIPT_URL)
+	first_page=$(wget -T 1 -q ${LOGIN_SCRIPT_URL} -O -)
+
+	if [ -z "$first_page" ]; then
+		echo "error: failed to get login page"
+		return '1'
+	fi
+
+	source_ip=$(echo "\'$first_page\'" | grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}')
+	echo -n "Source IP : "
+	echo $source_ip
+}
+
+# MAIN
+
+if [ $# -lt 1 ]; then
+	usage
+fi
+
+if (which "curl"); then
+	echo "we have curl"
+elif (which "wget"); then
+	echo "we have wget"
+else
+	echo "we need something"
+fi
+
+case $1 in
+"logout") ruw_logout;;
+
+"login")  ruw_login $@;;
+
+"status") ruw_status;;
+*)echo "error: unrecognized action"
+	usage;;
+esac
+
+exit 0
+
+
+#--- OLD CODE ---
+
+if [ $1 = "login" ]; then  
+	# Login
+  local ru_user ru_pass login_page uids
+	
+	if [ $# = 1 ]; then
+		read -p "Username: " ru_user
+		stty -echo
+		read -p "Password: " ru_pass; echo
+		stty echo
+	elif [ $# = 2 ]; then
+		ru_user=$2
+		#read in password
+		stty -echo
+		read -p "Password: " ru_pass; echo
+		stty echo
+	elif [ $# = 3 ]; then
+		ru_user=$2
+		ru_pass=$3
+	else
+		echo "error: incorrect parameters to login"
+		usage
+	fi
+  
+  login_page=$(curl -s	\
+   -F _FORM_SUBMIT="1"	\
+   -F which_form="reg"	\
+   -F destination=""	\
+   -F source=""		\
+   -F error=""		\
+   -F bs_name=$ru_user	\
+   -F bs_password=$ru_pass \
+   -c ruwireless.cookies \
+   $LOGIN_SCIPRT_URL)
+  
+  if echo $login_page | grep -q "action=logout"; then
+    echo "login successful"
+
+    echo -n "your ip is : "
+    echo $login_page | grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}'\
+	| cut -d $new_line -f 1
+
+    echo "your uid(s) is(are) : "
+    uids=$(echo $login_page | grep -E -o 'r=[[:alnum:]]{11}'\
+	| cut -d = -f 2)
+    echo -n "\t"
+    echo uids | cut -d $new_line -f 1
+    echo -n "\t"
+    echo uids | cut -d $new_line -f 2
+
+  elif echo $login_page | grep -q "already logged in"; then
+    echo " you are already logged in"
+    echo $login_page > ruwireless.alreadyin
+  else
+    local tmp
+    tmp=$(tempfile)
+    echo $login_page > tmp
+    echo " login parse failed, wrote login page to $tmp"
+  fi
+  # End login
+elif test $# = 1; then
+  if test $1 = "info"; then
+    # Check Status
+    echo " info unimplimented"
+    # End status
+  else
+    # Logout
+    local id ip logout_page logoutPopup_page
+    
+    logoutPopup_page=$(
+      curl -s \
+        -F action="logoutPopup" \
+ 	$URL)
+
+    ip=$(echo $logoutPopup_page | grep -E -o '([0-9]{1,3}\.){3}[0-9]{1,3}')
+    id=$(echo $logoutPopup_page | grep -E -o 'r=[[:alnum:]]{11}'\
+        | cut -d'='  -f 2)
+
+    ip=`echo $ip | cut -d $new_line -f 1`
+    id=`echo $id | cut -d $new_line -f 1`
+
+    echo $ip
+    echo $id
+
+    logout_page=$(curl -s \
+     -F action="logout"	\
+     -F source="$ip"	\
+     -F r="$id"	\
+     $URL)
+
+    echo $logout_page > ruwireless.logout
+    echo $logoutPopup_page > ruwireless.popup
+
+    if echo $logout_page | grep -q "This link is not valid for your current session."; then
+      echo " invalid logout link"
+    fi
+
+    if test $1 = "logout"; then
+      echo $logout_page
+    elif test $1 = "popup"; then
+      echo $logoutPopup_page
+    fi
+    # End logout
+  fi
+else
+  # Usage
+  echo "usage:"
+  echo "  login>  $0 <user> <pass>"
+  echo "  logout> $0 <login_id>"
+fi
+
