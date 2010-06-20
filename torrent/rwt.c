749fc947e9f951df0edf24d684940e1b5ca4da6a
John Mesmon <jmesmon@gmail.com>
2010-06-20 03:42:24 -0400

    rwt: add beginings of bencode parser.
    turns out we need to parse the bencoded data after all.
    Probably could stop once we get to the stuff we need, and
    append the rest as a raw string.

--- torrent/rwt.c
+++ torrent/rwt.c
@@ -9,6 +9,121 @@ static const char usage_str[] =
 "         show              - show current trackers\n"
 ;
 
+enum be_type {
+	BE_STR,
+	BE_DICT,
+	BE_INT,
+	BE_LIST
+};
+
+struct be_str {
+	size_t len;
+	char *str;
+};
+
+struct be_dict {
+	char **key;
+	be_node **val;
+};
+
+struct be_list {
+	size_t len;
+	struct be_node **nodes;
+}
+
+struct be_node {
+	enum be_type type;
+	union {
+		long i;
+		be_node **l;
+		be_dict *d;
+		be_str *s;
+	}
+};
+
+struct be_list *bdecode_list(const char *estr, size_t len, const char **ep)
+{
+	struct be_list *l = malloc(sizeof(*l));
+
+	/* assert(*estr == 'l'); */
+	for(estr += 1; estr != 'e'; estr) {
+		struct be_node *n = bdecode(estr + 1, &ep);
+		if (n) {
+		}		
+	}
+}
+
+/* parses a bencoded str from the encoded string buffer.
+ * on error : returns 0, estr = ep.
+ */
+struct be_str *bdecode_str(const char *estr, size_t len, const char **ep)
+{
+	if (len == 0) {
+		*ep = estr;
+		return 0;
+	}
+
+	const char *ppos = estr;
+	size_t slen = *ppos - '0';
+	for(;;) {
+		ppos++;
+		len--;
+		if(len <= 0) {
+			*ep = estr;
+		} else if (*ppos == ':') {
+			break;
+		}
+
+		slen *= 10;
+		slen += *ppos - '0';
+		/* TODO: detect overflow. */
+	}
+
+	/* ppos points to the ':' */
+	if (slen == 0 || slen > len) {
+		/* not a valid string. */
+		*ep = estr;
+		return 0;
+	}
+
+	char *str = malloc(len);
+	if (!str) {
+		*ep = estr;
+		return 0;
+	}
+
+	memcpy(str, ppos + 1, len);
+
+	struct be_str *bstr = malloc(sizeof(*bstr));
+	if (!bstr) {
+		*ep = estr;
+		free(str);
+		return 0;
+	}
+
+	bstr->len = slen;
+	bstr->str = str;
+	*ep = iep + 1 + slen;
+	return bstr;
+}
+
+struct be_node *bdecode(const char *estr, size_t len, const char **ep)
+{
+	switch(*estr) {
+	case 'd':
+		return bdecode_dict(estr, len, ep);
+	case 'i':
+		return bdecode_int(estr, len, ep);
+	case 'l':
+		return bdecode_list(estr, len, ep);
+
+	case '0'..'9':
+		return bdecode_str(estr, len, ep);
+	default:
+		return 0;
+	}
+}
+
 static void usage(char *name)
 {
 	fprintf(stderr, usage_str, name);
@@ -18,7 +133,6 @@ t_show(FILE *tf, int argc, char **argv)
 {
 
 }
-
 
 int main(int argc, char **argv)
 {
