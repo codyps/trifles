a9ba0b0a43a8a7af9356c12ebc6a3a833e92a8d3
John Mesmon <jmesmon@gmail.com>
2010-07-11 20:33:14 -0400

    header reorg.

--- conpal.c
+++ conpal.c
@@ -1,11 +1,12 @@
-#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include <sys/ioctl.h>
-#include <sys/types.h>
 
 #include <linux/kd.h>
 
