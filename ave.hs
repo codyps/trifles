22608848271df016bffb9a8c3fc7b5ae49783930
John Mesmon <jmesmon@gmail.com>
2010-10-13 00:32:22 -0400

    ave.hs: foldl generalization.

--- ave.hs
+++ ave.hs
@@ -1,7 +1,12 @@
---myave :: (Num a) => [a] -> a
-myave xs = myave_ xs 0 0
+--myave :: (Fractional a) => [a] -> a
+average1 xs = myave_ xs 0 0
 
---myave_ :: (Num a, Fractional b) => [a] -> a -> b -> a
+--myave_ :: (Fractional a) => [a] -> a -> b -> a
 myave_ []     acc ct = acc / ct
-myave_ (x:xs) acc ct =
-	myave_ xs (acc + x) (ct + 1)
+myave_ (x:xs) acc ct = myave_ xs (acc + x) (ct + 1)
+
+average2 xs =
+	let (acc,ct) =
+		foldl (\(acc,ct) x -> (acc + x, ct + 1)) (0,0) xs in
+	acc / ct
+	
