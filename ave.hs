64919a9c6bb96f018cbf9d7e001edf025afac7ee
John Mesmon <jmesmon@gmail.com>
2010-10-16 21:45:22 -0400

    ave.hs: tweaks.

--- ave.hs
+++ ave.hs
@@ -1,9 +1,9 @@
 --myave :: (Fractional a) => [a] -> a
 average1 xs = myave_ xs 0 0
-
---myave_ :: (Fractional a) => [a] -> a -> b -> a
-myave_ []     acc ct = acc / ct
-myave_ (x:xs) acc ct = myave_ xs (acc + x) (ct + 1)
+	where
+		--myave_ :: (Fractional a) => [a] -> a -> b -> a
+		myave_ []     acc ct = acc / ct
+		myave_ (x:xs) acc ct = myave_ xs (acc + x) (ct + 1)
 
 average2 xs =
 	let (acc,ct) =
