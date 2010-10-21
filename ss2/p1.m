ed39c055e960a6d90ce9ba0739413fdc73b3ac37
John Mesmon <jmesmon@gmail.com>
2010-10-21 00:10:44 -0400

    ss2: semi complete.

--- ss2/p1.m
+++ ss2/p1.m
@@ -1,17 +1,32 @@
 clf
 clear
-clc
+%clc
 
-td = 5; Tmax = 25; a = 0.5; tp = 10; T = Tmax/1000;
+td = 0;
+%td = 5;
+Tmax = 25; a = 0.5; tp = 10; T = Tmax/1000;
 t = 0:T:Tmax;
 h = a .* exp(-a .* t);
 f = rectpuls(t - tp /2 - td,tp);
 
-yexact = exp(-a .* (t)) .* (exp(a .* min(tp + td,t)) - 1);
+yexact = exp(-a .* (t - td)) .* (exp(a .* min(tp,t - td)) - 1);
+yexact(yexact < 0) = 0;
 y = T .* conv(double(h),double(f));
 
 y = y(1:length(t));
 
 plot(t,f,t,y,t,yexact)
 legend({'f','y_{approx}','y_{exact}'})
+xlabel('Time (seconds)');
+ylabel('Output Value (unitless)');
+%title('Time Invariance Property Demonstation, Shifted');
+title('Time Invariance Property Demonstation, Unshifted');
 
+paper_size = [3 3.5];
+set (gcf, "paperunits", "inches")
+set (gcf, "papertype", "<custom>")
+set (gcf, "papersize", paper_size)
+set (gcf, "paperposition", [0, 0, paper_size])
+
+%print('-dpng', 'P1_A.png');
+print('-dpng', 'P1_B.png');
--- ss2/p2.m
+++ ss2/p2.m
@@ -1,18 +1,22 @@
 clf
 clear
-clc
-
+%clc
 
 Tmax = 100; T= Tmax / 1000; t = 0:T:Tmax;
 f = sin(3*t) .* rectpuls(t - 30/2,30) + ...
     sin(2*t) .* rectpuls(t-30 - 40/2,40) + ...
     sin(3*t) .* rectpuls(t-70 - 30 /2,30);
-w = 2;
+wo = 2;
+%a = .1;
 a = .3;
-wr = sqrt(w^2 - a^2 /4);
+wr = sqrt(wo.^2 - a.^2 / 4);
 
 g = a * exp(-a * t / 2) .* (cos(wr * t) - a/(2*wr) * sin(wr*t));
 h = dirac(t) - g;
+
+H = @(s) (s.^2 + wo .^2) ./ (s.^2 + a .* s + wo .^2);
+w = 0:5/1000:5;
+aH = abs(H(I.*w)).^2;
 
 y1 = T * conv(g,f);
 y1 = y1(1:length(t));
@@ -21,21 +25,32 @@ y = f - y1;
 subplot(2,2,1);
 plot(t,f);
 title('input signal, f(t)');
+xlabel('Time (seconds)');
 axis([0, 100, -2, 2]);
 grid('on');
 
 subplot(2,2,2);
 plot(t,y);
 title('output signal, y(t)');
+xlabel('Time (seconds)');
 axis([0, 100, -2, 2]);
 grid('on');
 
 subplot(2,2,3);
+plot(w,aH);
 title('notch filter magnitude responce, |H(\omega)|^2');
-plot(t,h);
 axis([0,5,0,1.1]);
 
 subplot(2,2,4);
+plot(t,h);
 title('notch filter impulse response');
-plot(t,tf(h))
+xlabel('Time (seconds)');
 axis([0,100,-.4,.3]);
+
+paper_size = [3 3.5];
+set (gcf, "paperunits", "inches")
+set (gcf, "papertype", "<custom>")
+set (gcf, "papersize", paper_size)
+set (gcf, "paperposition", [0, 0, paper_size])
+print('-dpng', 'P2_A.png');
+%print('-dpng', 'P2_B.png');
