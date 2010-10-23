5715f2bdd62e2c51185f4f675072611dae3e5a33
John Mesmon <jmesmon@gmail.com>
2010-10-23 19:17:08 -0400

    pm_timer: modify for nicer output & internals.

--- pm_timer.c
+++ pm_timer.c
@@ -6,37 +6,35 @@
 
 typedef unsigned int u32;
 static unsigned short pmtmr_ioport;
-static int cnt;
 
-#define ACPI_PM_MASK 0xFFFFFF /* limit it to 24 bits */
-
-static u32 read_pmtmr(void)
+static int read_pmtmr(void)
 {
-	u32 v1=0,v2=0,v3=0;
+	u32 v1, v2, v3;
 	/* It has been reported that because of various broken
 	 * * chipsets (ICH4, PIIX4 and PIIX4E) where the ACPI PM time
 	 * * source is not latched, so you must read it multiple
 	 * * times to insure a safe value is read.
 	 * */
-	cnt = 0;
-	do {
-		v1 = inl(pmtmr_ioport);
-		v2 = inl(pmtmr_ioport);
-		v3 = inl(pmtmr_ioport);
-		cnt++;
-	} while ((v1 > v2 && v1 < v3) || (v2 > v3 && v2 < v1)
-			|| (v3 > v1 && v3 < v2));
+	v1 = inl(pmtmr_ioport);
+	v2 = inl(pmtmr_ioport);
+	v3 = inl(pmtmr_ioport);
 
-	/* mask the output to 24 bits */
-	return v2 & ACPI_PM_MASK;
+	if ((v1 > v2 && v1 < v3) || (v2 > v3 && v2 < v1)
+			|| (v3 > v1 && v3 < v2))
+		return -1;
+	else
+		return 0;
 }
 
 int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc < 2)
-		error(1, 0, "Usage: %s pmtmr_port\n", argv[0]);
+	if (argc < 2) {
+		fprintf(stderr, "usage: %s <pm timer port>\n",
+				argv[0]?argv[0]:"pm_timer_test");
+		return -2;
+	}
 
 	pmtmr_ioport = strtoul(argv[1], NULL, 0);
 	if ((pmtmr_ioport & 0xff) != 0x08)
@@ -45,12 +43,18 @@ int main(int argc, char *argv[])
 	if (iopl(3) < 0)
 		error(1, errno, "iopl");
 
-	for (i = 0; i < 10000000; i++) {
-		read_pmtmr();
-		if (cnt > 1)
-			error(1, 0, "Detect PM-Timer Bug\n");
-		if ((i % 100000) == 0)
-			printf("%d\n", i);
+	for (i = 1; i <= 10000000; i++) {
+		if (read_pmtmr() < 0) {
+			fprintf(stderr,
+				"\nread %d failed, pm-timer bug detected\n",
+				i);
+			return -1;
+		}
+		if ((i % 100000) == 0) {
+			printf("\rsuccesful reads: %d", i);
+			fflush(stdout);
+		}
 	}
+	putchar('\n');
 	return 0;
 }
