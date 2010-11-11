ba26815bf403147f7d66a82672a74544561da593
John Mesmon <jmesmon@gmail.com>
2010-11-10 21:05:50 -0500

    partial as_insert.

--- bytes/ifibin_to_ihex.c
+++ bytes/ifibin_to_ihex.c
@@ -40,6 +40,7 @@ void error_at_line(int status, int errnum, const char *filename,
 
 struct addr_space {
 	unsigned long region_ct;
+	unsigned long region_mem_sz;
 	struct mem_region *regions;
 };
 
@@ -102,10 +103,37 @@ int mr_append(struct mem_region *mr, void *data, size_t data_len)
 	return 0;
 }
 
-int as_insert(struct addr_space *as, uint32_t addr, char *data,
+int as_insert(struct addr_space *as, unsigned long addr, char *data,
               size_t data_len)
 {
 #warning "unimplimended"
+
+	/* Find a memory region containing the data we want to add
+	 * or abutting our region */
+	unsigned long addr_end = addr + data_len;
+	size_t i = 0;
+	for(i = 0; i < as->region_ct; i++) {
+		struct mem_region *mr = as->regions + i;
+		unsigned long mr_addr = mr->addr;
+		size_t mr_len = mr->used_len;
+		unsigned long mr_addr_end = mr_addr + mr_len;
+
+		/* the added data is below exsisting data */
+		bool abut_low  = (addr < mr_addr)
+		                 && (addr_end > mr_addr);
+
+		/* the added data is above exsisting data */
+		bool abut_high = (mr_addr < addr)
+		                 && (mr_addr_end > addr);
+
+		if ( abut_high || abut_low ) {
+			/* collision */
+
+		}
+	}
+
+	/* no collision, append */
+
 	return -1;
 }
 
