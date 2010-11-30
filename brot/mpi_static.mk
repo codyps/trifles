9575c00986c4a654b7de1044e2611c7a3ba394cb
Cody Schafer <cpschafer@spring.rutgers.edu>
2010-11-30 18:27:54 -0500

    brot: build sys work.

--- brot/mpi_static.mk
+++ brot/mpi_static.mk
@@ -1,23 +1,6 @@
 SRC = mandelbrot_mpi_static.c
-
-CC=/usr/local/mpich/bin/mpicc
-RM=rm -f
-
-CFLAGS = -ggdb
-override CFLAGS+= -Wall -pipe -lX11 -I/usr/include/X11 -L/usr/lib -lm
-
 BIN = brot_mpi_static
 
-.PHONY: all
-all: build
-	
-.PHONY: build
-build: $(BIN)
+CC=/usr/local/mpich/bin/mpicc
 
-.PHONY: clean
-clean:
-	$(RM) $(BIN)
-
-$(BIN): $(SRC)
-	$(CC) $(CFLAGS) -o $@ --combine $<
-
+include common.mk
--- brot/openmp.mk
+++ brot/openmp.mk
@@ -1,23 +1,7 @@
 SRC = mandelbrot_omp.c
-
-CC=gcc
-RM=rm -f
-
-CFLAGS = -ggdb
-override CFLAGS+= -Wall -pipe -fopenmp -lX11 -I/usr/include/X11 -L/usr/lib -lm
-
 BIN = brot_omp
 
-.PHONY: all
-all: build
-	
-.PHONY: build
-build: $(BIN)
+CC=gcc
+override CFLAGS += -fopenmp
 
-.PHONY: clean
-clean:
-	$(RM) $(BIN)
-
-$(BIN): $(SRC)
-	$(CC) $(CFLAGS) -o $@ --combine $<
-
+include common.mk
