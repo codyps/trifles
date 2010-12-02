e00d8224ddd18e3678c2a55f1c90c90a5cfdca24
John Mesmon <jmesmon@gmail.com>
2010-12-02 17:34:54 -0500

    dsd6: p14: done

--- verilog/hw6/p14.v
+++ verilog/hw6/p14.v
@@ -2,7 +2,7 @@
 /* Problem # 14 */
 /* 64k memory, 8 segments, 16 bit addr */
 module segment_decode(output [$clog2(8)-1:0] seg, input [15:0]addr);
-	assign seg = addr[15:15-$clog2(8)];
+	assign seg = addr[15:15-$clog2(8)+1];
 endmodule
 
 module tb();
@@ -13,8 +13,11 @@ module tb();
 
 	integer i;
 	initial begin
-		for(i = 0; i < $cpow(2,16); i = i + 1) begin
+		$dumpfile("p14.vcd");
+		$dumpvars(0,tb);
+		for(i = 0; i < (2 ** 16); i = i + 1) begin
 			addr = i;
+			#10;
 		end
 	end
 endmodule
