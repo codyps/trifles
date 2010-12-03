9b51300bfce5905f0bd429cf56146e68f07e13e0
John Mesmon <jmesmon@gmail.com>
2010-12-02 19:30:28 -0500

    dsd6: submitted :(

--- verilog/hw6/p13.v
+++ verilog/hw6/p13.v
@@ -1,44 +1,75 @@
-
 /* Problem # 13 */
-module freq_div #(parameter divisor_width = 8, parameter duty_cycle_width = 8)
+module freq_div 
+	#(parameter divisor_width = 8,
+	parameter duty_cycle_width = divisor_width)
+
 	(output reg out_clk,
 	input [divisor_width-1:0] divisor,
 	input [duty_cycle_width-1:0] duty_cycle,
 	input reset_l, in_clk);
 	
 	reg [divisor_width-1:0] div_track;
-	reg [duty_cycle_width-1:0] duty_track;
-	reg [$clog2(duty_cycle_width)-1:0] duty_div;
-	
-	always @(div_track) begin
-		if (div_track == 0)
-			duty_track = 0;
-	end
-	
+
 	always @(posedge in_clk) begin
-		
-		if (div_track == divisor) begin
-			out_clk <= 1;
-			div_track <= 0;
+		div_track = div_track + 1;
+	end
+
+	reg ct;
+	always @(div_track) begin
+		if (out_clk) begin
+			if (duty_cycle[duty_cycle_width-1])
+				ct = divisor + duty_cycle + 1;
+			else
+				ct = divisor - duty_cycle + 1;
 		end else begin
-			div_track  <= div_track  + 1;
+			if (duty_cycle[duty_cycle_width-1])
+				ct = divisor - duty_cycle + 1;
+			else
+				ct = divisor + duty_cycle + 1;
 		end
-		
-		if (duty_track == duty_cycle) begin
-			out_clk <= ~out_clk;
-			duty_track <= 0;
-		end else begin
-			duty_track <= duty_track + 1;
+
+		if ( (div_track == ct) || (div_track > ct) ) begin
+			out_clk = ~out_clk;
+			div_track = 0;
 		end
 	end
-	
+
 	always @(reset_l) begin
 		if (reset_l == 0) begin
 			out_clk <= 0;
 			div_track <= 0;
-			duty_track <= 0;
 		end
 	end
 	
 endmodule
 
+module tb();
+	wire out_clk;
+	reg [7:0] divisor, duty_cycle;
+	reg reset_l, in_clk;
+
+	freq_div dev(out_clk, divisor, duty_cycle, reset_l, in_clk);
+
+	initial begin
+		in_clk = 0;
+		forever #4 in_clk = ~in_clk;
+	end
+	
+	integer i;
+	initial begin 
+		$dumpfile("p13.vcd");
+		$dumpvars(0,tb);
+		fork
+			#0 reset_l = 0;
+			#0 divisor = 1;
+			#0 duty_cycle = 1;
+			#2 reset_l = 1;
+
+			#100 divisor = 10;
+			
+			#300 duty_cycle = 3; 
+
+			#500 $finish;
+		join
+	end
+endmodule
--- verilog/hw6/p18.v
+++ verilog/hw6/p18.v
@@ -1,4 +1,3 @@
-
 /* Problem # 18 */
 module Control_Unit(output reg done, Ld_AR_BR, Div_AR_x2_CR, Mul_BR_x2_CR, Clr_CR,
 	input reset_b, start, AR_gt_0, AR_lt_0, clk);
@@ -9,25 +8,38 @@ module Control_Unit(output reg done, Ld_AR_BR, Div_AR_x2_CR, Mul_BR_x2_CR, Clr_C
 			Mul_BR_x2_CR = 0;
 			Div_AR_x2_CR = 0;
 			Clr_CR = 0;
+			done = 0;
 		end else begin
 			if (AR_gt_0) begin
 				Ld_AR_BR = 0;
 				Mul_BR_x2_CR = 1;
 				Div_AR_x2_CR = 0;
 				Clr_CR = 0;
+				done = 0;
 			end else if (AR_lt_0) begin
 				Ld_AR_BR = 0;
 				Mul_BR_x2_CR = 0;
 				Div_AR_x2_CR = 1;
 				Clr_CR = 0;
+				done = 0;
 			end else begin
 				Ld_AR_BR = 0;
 				Mul_BR_x2_CR = 0;
 				Div_AR_x2_CR = 0;
 				Clr_CR = 1;
+				done = 1;
 			end
 		end
 	end
+
+	always @(reset_b)
+		if (reset_b == 0) begin
+			Ld_AR_BR = 0;
+			Mul_BR_x2_CR = 0;
+			Div_AR_x2_CR = 0;
+			Clr_CR = 0;
+			done = 0;
+		end
 	
 endmodule
 
@@ -57,4 +69,50 @@ module Datapath_Unit(output reg signed[15:0] CR, output reg AR_gt_0, AR_lt_0,
 	end
 	
 endmodule
-	
+
+module tb();
+	/* Outputs */
+	wire [15:0] CR;
+	wire done;
+
+	/* Interconnects */
+	wire AR_gt_0, AR_lt_0;
+	wire Ld_AR_BR, Div_AR_x2_CR, Mul_BR_x2_CR, Clr_CR;
+
+	/* Inputs */
+	reg clk;
+	reg [15:0] Data_BR, Data_AR;
+
+	reg reset_b;
+	reg start;
+
+	Datapath_Unit ddev (CR, AR_gt_0, AR_lt_0, Data_AR, Data_BR, 
+		Ld_AR_BR, Div_AR_x2_CR, Mul_BR_x2_CR, Clr_CR, clk);
+
+	Control_Unit cdev(done, Ld_AR_BR, Div_AR_x2_CR, Mul_BR_x2_CR, Clr_CR,
+		reset_b, start, AR_gt_0, AR_lt_0, clk);
+
+	reg [35:0] i;
+	initial begin
+		$dumpfile("p18.vcd");
+		$dumpvars(0,tb);
+
+		clk = 0;
+		reset_b = 0;
+		#2 reset_b = 1;
+
+		for(i = 0; i < 2**32-1; i = i + 3) begin
+			Data_BR = i[15:0];
+			Data_AR = i[31:16];
+			
+			start = 1;
+			#10 clk = 1;
+			#10 clk = 0;
+
+			while(~done) begin
+				#10 clk = 1;
+				#10 clk = 0;
+			end
+		end
+	end
+endmodule
