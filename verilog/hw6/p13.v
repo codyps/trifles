ffae0e6f96fe35e445c7e870c31deeae0931ba74
Cody Schafer <cpschafer@gmail.com>
2010-12-04 05:14:33 -0500

    hw6: p13 works.

--- verilog/hw6/p13.v
+++ verilog/hw6/p13.v
@@ -1,5 +1,9 @@
 /* Problem # 13 */
-module freq_div 
+`define max(a,b)((a) > (b) ? (a) : (b))
+
+/* For things to make sense, divisor_width should be larger than
+ * duty_cycle_width */
+module freq_div
 	#(parameter divisor_width = 8,
 	parameter duty_cycle_width = divisor_width)
 
@@ -10,27 +14,28 @@ module freq_div
 	
 	reg [divisor_width-1:0] div_track;
 
-	always @(posedge in_clk) begin
-		div_track = div_track + 1;
+	always @(in_clk) begin
+		div_track <= div_track + 1;
 	end
 
-	reg ct;
+	reg [divisor_width:0] ct;
+	wire d_high = duty_cycle[duty_cycle_width - 1];
+	wire [duty_cycle_width - 2:0] d_low  = duty_cycle[duty_cycle_width-2:0];
 	always @(div_track) begin
-		if (out_clk) begin
-			if (duty_cycle[duty_cycle_width-1])
-				ct = divisor + duty_cycle + 1;
+		if (out_clk)
+			if (d_high)
+				ct = divisor + d_low;
 			else
-				ct = divisor - duty_cycle + 1;
-		end else begin
-			if (duty_cycle[duty_cycle_width-1])
-				ct = divisor - duty_cycle + 1;
+				ct = divisor - d_low;
+		else
+			if (d_high)
+				ct = divisor - d_low;
 			else
-				ct = divisor + duty_cycle + 1;
-		end
+				ct = divisor + d_low;
 
-		if ( (div_track == ct) || (div_track > ct) ) begin
-			out_clk = ~out_clk;
-			div_track = 0;
+		if (div_track >= ct) begin
+			out_clk <= ~out_clk;
+			div_track <= 0;
 		end
 	end
 
@@ -62,14 +67,16 @@ module tb();
 		fork
 			#0 reset_l = 0;
 			#0 divisor = 1;
-			#0 duty_cycle = 1;
+			#0 duty_cycle = 'b10000000; 
 			#2 reset_l = 1;
+			#50 divisor = 2;
+			#100 divisor = 0;
+			#200 divisor = 10;
 
-			#100 divisor = 10;
-			
-			#300 duty_cycle = 3; 
+			#300 duty_cycle = 'b10000001;	
+			#400 duty_cycle = 'b10000101;	
 
-			#500 $finish;
+			#600 $finish;
 		join
 	end
 endmodule
