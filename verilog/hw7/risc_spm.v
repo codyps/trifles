1d96f6f16e16ed3641e2ce143c69f18620e807d1
John Mesmon <jmesmon@gmail.com>
2010-12-08 16:56:46 -0500

    dsd7: control unit

--- verilog/hw7/risc_spm.v
+++ verilog/hw7/risc_spm.v
@@ -1,8 +1,8 @@
 module risc_spm #(parameter word_sz=8, sel1_sz=3, sel2_sz=2)
 	(	input clk, rst);
 
-	wire [sel1_sz-1:0]sel1_bus_mux;
-	wire [sel2_sz-1:0]sel2_bus_mux;
+	wire [sel1_sz-1:0]bus1_sel_mux;
+	wire [sel2_sz-1:0]bus2_sel_mux;
 
 	/* Data */
 	wire zero;
@@ -22,8 +22,8 @@ module proc_unit #(parameter word_sz=8, sel1_sz=3, sel2_sz=2, op_sz=4)
 		output flag_z,
 		input [word_sz-1:0] mem_word,
 		input load_r0, load_r1, load_r2, load_r3, load_pc, inc_pc,
-		input [sel1_sz-1:0] sel1_bus_mux,
-		input [sel2_sz-1:0] sel2_bus_mux,
+		input [sel1_sz-1:0] bus1_sel_mux,
+		input [sel2_sz-1:0] bus2_sel_mux,
 		input load_ir, load_add_r, load_reg_y, load_reg_z,
 		input clk, rst);
 
@@ -51,8 +51,8 @@ module proc_unit #(parameter word_sz=8, sel1_sz=3, sel2_sz=2, op_sz=4)
 
 	prgm_cnt  pc(pc_count, bus_2, load_pc, inc_pc, clk, rst);
 
-	mux_5ch   mux_1(bus_1, out_r0, out_r1, out_r2, out_r3, pc_count, sel1_bus_mux);
-	mux_3ch   mux_2(bus_2, alu_out, bus_1, mem_word, sel2_bus_mux);
+	mux_5ch   mux_1(bus_1, out_r0, out_r1, out_r2, out_r3, pc_count, bus1_sel_mux);
+	mux_3ch   mux_2(bus_2, alu_out, bus_1, mem_word, bus2_sel_mux);
 
 	alu_risc alu(alu_zero_flag, alu_out, y_vlue, bus_1, opcode);
 
@@ -140,6 +140,211 @@ module alu_risc #(parameter word_sz=8, op_sz=4)
 endmodule
 
 module ctrl_unit #(word_sz=8, op_sz=4, state_sz=4, src_sz=2, dst_sz=2, sel1_sz=3, sel2_sz=2)
-	();
+	(	output reg load_r0, load_r1, load_r2, load_r3, load_r4, load_pc, inc_pc,
+		output [sel1_sz-1:0] bus1_sel_mux,
+		output reg load_ir, load_add_r, load_reg_y, load_reg_z,
+		output [sel2_sz-1:0] bus2_sel_mux,
+		output write,
+		input [word_sz-1:0] instr,
+		input zero,
+		input clk, rst);
 
+	wire [op_sz-1:0] opcode = instr[word_s-1:word_sz-op_sz];
+	wire [src_sz-1:0] src = instr[src_sz + dst_sz - 1: dst_sz];
+	wire [dst_sz-1:0] dst = instr[dst_sz-1:0];
+
+	assign bus1_sel_mux = sel_r0 ? 0 :
+				sel_r1 ? 1 :
+				sel_r2 ? 2 :
+				sel_r3 ? 3 :
+				sel_pc ? 4 : {sel1_sz{'bx}};
+
+	assign bus2_sel_mux = sel_alu ? 0 :
+				sel_bus1 ? 1 :
+				sel_mem ? 2: {sel2_sz{'bx}};
+
+	always @(posedge clk or negedge rst) begin : state_transitions
+		if (~rst)
+			state <= S_idle;
+		else
+			state <= next_state;
+	end
+
+	always @(state or opcode or src or dst or zero) begin : output_and_next_state
+		sel_r0 = 0; sel_r1 = 0; sel_r2 = 0; sel_r3 = 0; sel_pc = 0;
+		load_r0 = 0; load_r1 = 0; load_r2 = 0; load_r3 = 0; load_pc = 0;
+
+		load_ir = 0; load_add_r = 0; load_reg_y = 0; load_reg_z = 0;
+		inc_pc = 0;
+		bus1_sel = 0;
+		sel_alu = 0;
+		sel_mem = 0;
+		write = 0;
+		err_flag = 0;
+		next_state = state;
+		case (state)
+			S_idle: next_state = S_fet1;
+			S_fet1: begin
+				next_state = S_fet2;
+				sel_pc = 1;
+				bus1_sel = 1;
+				load_add_r = 1;
+			end
+			S_fet2: begin
+				next_state = S_dec;
+				sel_mem = 1;
+				load_ir = 1;
+				inc_pc = 1;
+			end
+			S_dec: case (opcode)
+				op.NOP: next_state = S_fet1;
+
+				op.ADD, op.SUB, op.AND: begin
+					next_state = S_ex1;
+					bus1_sel = 1;
+					load_reg_y = 1;
+					case (src)
+						R0: sel_r0 = 1;
+						R1: sel_r1 = 1;
+						R2: sel_r2 = 1;
+						R3: sel_r3 = 1;
+						default err_flag = 1;
+					endcase
+					end /* op.ADD, op.SUB, op.AND */
+
+				op.NOT: begin
+					next_state = S_fet1;
+					load_reg_z = 1;
+					bus1_sel = 1;
+					sel_alu = 1;
+					case (src)
+						R0: sel_r0 = 1;
+						R1: sel_r1 = 1;
+						R2: sel_r2 = 1;
+						R3: sel_r3 = 1;
+						default err_flag = 1;
+					endcase
+					case (dst)
+						R0: load_r0 = 1;
+						R1: load_r1 = 1;
+						R2: load_r2 = 1;
+						R3: load_r3 = 1;
+						default err_flag = 1;
+					endcase
+				end /* op.NOT */
+
+				op.RW: begin
+					next_state = S_rd1;
+					sel_pc = 1;
+					bus1_sel = 1;
+					load_add_r = 1;
+				end
+
+				op.WR: begin
+					next_state = S_wr1;
+					sel_pc = 1;
+					bus1_sel = 1;
+					load_add_r = 1;
+				end
+
+				op.BR: begin
+					next_state = S_br1;
+					sel_pc = 1;
+					bus1_sel = 1;
+					load_add_r = 1;
+				end
+				op.BRZ: if (zero) begin
+					next_state = S_br1;
+					sel_pc = 1;
+					bus1_sel = 1;
+					load_add_r = 1;
+				end else begin
+					next_state = S_fet1;
+					inc_pc = 1;
+				end
+				default: next_state = S_halt;
+			endcase /* opcode */
+			
+			S_ex1: begin
+				next_state = S_fet1;
+				load_reg_z = 1;
+				sel_alu = 1;
+				case (dst)
+					R0: begin sel_r0 = 1; load_r0 = 1; end
+					R1: begin sel_r1 = 1; load_r1 = 1; end
+					R2: begin sel_r2 = 1; load_r2 = 1; end
+					R3: begin sel_r3 = 1; load_r3 = 1; end
+					default: err_flag = 1;
+				endcase
+			end
+
+			S_rd1: begin
+				next_sate = S_rd2;
+				sel_mem = 1;
+				load_add_r = 1;
+				inc_pc = 1;
+			end
+
+			S_wr1: begin
+				next_state = S_wr1;
+				sel_mem = 1;
+				load_add_r = 1;
+				inc_pc = 1;
+			end
+
+			S_rd2: begin
+				next_state = S_fet1;
+				sel_mem = 1;
+				case (dst)
+					R0: load_r0 = 1;
+					R1: load_r1 = 1;
+					R2: load_r2 = 1;
+					R3: load_r3 = 1;
+					default: err_flag = 1;
+				endcase
+			end
+
+			S_wr2: begin
+				next_state = S_fet1;
+				write = 1;
+				case (src)
+					R0: sel_r0 = 1;
+					R1: sel_r1 = 1;
+					R2: sel_r2 = 1;
+					R3: sel_r3 = 1;
+					default: err_flag = 1;
+				endcase
+			end
+
+			S_br1: begin
+				next_state = S_br2;
+				sel_mem = 1;
+				load_add_r = 1;
+			end
+
+			S_br2: begin
+				next_state = S_fet1;
+				sel_mem = 1;
+				load_pc = 1;
+			end
+
+			S_halt: next_state = S_halt;
+
+			default: next_state = S_idle;
+		endcase /* state */
+	end /* always @(state or opcode or src or dst or zero)  */
+endmodule
+
+module mem_unit #(parameter word_sz = 8, mem_sz = 256)
+	(	output [word_sz-1:0] data_out,
+		input [word_sz-1:0] data_in, address,
+		input clk, write);
+
+	reg [word_sz-1:0] mem[mem_sz-1:0];
+
+	assign data_out = mem[address];
+
+	always @(posedge clk)
+		if (write)
+			mem[address] <= data_in;
 endmodule
