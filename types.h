aa1622730e2eb4d9d0616531e395ea7646a3ac8c
Cody Schafer <cpschafer@gmail.com>
2011-03-19 16:38:27 -0400

    add list.

---
+++ types.h
@@ -0,0 +1,27 @@
+#ifndef _LINUX_TYPES_H
+#define _LINUX_TYPES_H
+
+#ifdef __CHECKER__
+#define __bitwise__ __attribute__((bitwise))
+#else
+#define __bitwise__
+#endif
+#ifdef __CHECK_ENDIAN__
+#define __bitwise __bitwise__
+#else
+#define __bitwise
+#endif
+
+struct list_head {
+	struct list_head *next, *prev;
+};
+
+struct hlist_head {
+	struct hlist_node *first;
+};
+
+struct hlist_node {
+	struct hlist_node *next, **pprev;
+};
+
+#endif /* _LINUX_TYPES_H */
