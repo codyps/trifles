43968a515f2e413c4e0bac390f597d5303a722c8
Cody Schafer <cpschafer@gmail.com>
2011-05-07 04:34:00 -0400

    google code: p1

---
+++ gc/p1.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include "list.h"
+
+#define L_TRACK_INIT(name) { .acts = LIST_HEAD_INIT(name.acts), .pos = {1, 1} }
+#define L_TRACK(name) struct l_track name = L_TRACK_INIT(name)
+
+struct act {
+	bool blue;
+	unsigned goal_pos;
+	struct list_head list;
+};
+
+struct l_track {
+	struct list_head acts;
+#define P_ORANGE 0
+#define P_BLUE 1
+	unsigned pos[2];
+	unsigned time_total;
+};
+
+
+struct act *find_next_other(struct act *cur, struct list_head *head)
+{
+	struct act *cur2;
+	bool blue = cur->blue;
+	list_for_each_entry(cur2, head, list) {
+		if (blue != cur2->blue)
+			return cur2;
+	}
+
+	/* XXX: oh god this will blow up in my face */
+	return NULL;
+}
+
+unsigned abs_diff(unsigned goal, unsigned cur)
+{
+	//printf("goal: %u cur: %u\n", goal, cur);
+	if (goal > cur)
+		return goal - cur;
+	else
+		return cur - goal;
+}
+
+unsigned move_closer_by(unsigned cur_pos, unsigned goal_pos, unsigned time_passed)
+{
+	if (cur_pos > goal_pos) {
+		return cur_pos - time_passed;
+	} else {
+		return cur_pos + time_passed;
+	}
+}
+
+int eval_line(struct l_track *lt)
+{
+	struct act *cur;
+	struct act *n;
+	struct act *oth = list_entry(lt->acts.next, typeof(*oth), list);
+	list_for_each_entry_safe(cur, n, &lt->acts, list) {
+		if (cur == oth) {
+			oth = find_next_other(cur, &lt->acts);
+		}
+
+		unsigned cur_tt = abs_diff(cur->goal_pos, lt->pos[cur->blue]) + 1;
+		if (oth) {
+			unsigned oth_tt =
+				abs_diff(oth->goal_pos, lt->pos[oth->blue]) + 1;
+
+			/* XXX: the edge case is hard due to button presses being a
+			 * seperate move from moving */
+			if (oth_tt <= cur_tt) {
+				lt->pos[oth->blue] = oth->goal_pos;
+			} else {
+				/* adj other pos closer to goal by cur_tt */
+				lt->pos[oth->blue] = move_closer_by(
+						lt->pos[oth->blue],
+						oth->goal_pos,
+						cur_tt);
+			}
+		}
+
+		lt->pos[cur->blue] = cur->goal_pos;
+		lt->time_total += cur_tt;
+		//printf("\ttt: %u (+%u)\n", lt->time_total, cur_tt);
+
+		list_del(&cur->list);
+		free(cur);
+	}
+
+	return 0;
+}
+
+int parse_line(FILE *in, struct l_track *lt)
+{
+	unsigned acts;
+	int ret = fscanf(in, "%u ", &acts);
+	if (ret != 1) {
+		perror("bleh");
+		return -2;
+	}
+
+	unsigned i;
+	for (i = 0; i < acts; i ++) {
+		char color_char;
+		unsigned goal_pos;
+		ret = fscanf(in, "%c %u ", &color_char, &goal_pos);
+		if (ret != 2) {
+			perror("Belh");
+			printf("matched %u %d\n", i, ret);
+			return -4;
+		}
+
+		struct act *nact = malloc(sizeof(*nact));
+
+		nact->blue = color_char == 'B';
+		nact->goal_pos = goal_pos;
+
+		list_add_tail(&nact->list, &lt->acts);
+	}
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	unsigned cases;
+	int ret = scanf("%u\n", &cases);
+	if (ret != 1)
+		return -1;
+
+	unsigned i;
+	for (i = 0; i < cases; i ++) {
+		L_TRACK(lt);
+
+		ret = parse_line(stdin, &lt);
+		if (ret < 0)
+			return ret;
+
+		ret = eval_line(&lt);
+		if (ret < 0)
+			return ret;
+
+		printf("Case #%u: %u\n", i + 1, lt.time_total);
+	}
+
+	return 0;
+}
---
+++ gc/penny.h
@@ -0,0 +1,58 @@
+#ifndef PENNY_PENNY_H_
+#define PENNY_PENNY_H_
+
+#include <stddef.h>
+
+#define ARRAY_SIZE(x) (sizeof(x)/sizeof((x)[0]))
+
+#define FIELD_SIZE(s,f) (sizeof((s *)0->f))
+
+#define typeof_field(s, f) typeof((s *)0->f)
+
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ * @ptr:	the pointer to the member.
+ * @type:	the type of the container struct this is embedded in.
+ * @member:	the name of the member within the struct.
+ *
+ */
+#define container_of(ptr, type, member) ({			\
+	const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
+	(type *)( (char *)__mptr - offsetof(type,member) );})
+
+/* memory barrier */
+#define barrier() __asm__ __volatile__ ("":::"memory")
+
+/* prefetch for reading */
+#define prefetch(x)  __builtin_prefetch(x)
+
+/* prefetch for writing */
+#define prefetchw(x) __builtin_prefetch(x,1)
+
+#ifndef htonll
+# ifdef _BIG_ENDIAN
+#  define htonll(x)   (x)
+#  define ntohll(x)   (x)
+# else
+#  define htonll(x)   ((((uint64_t)htonl(x)) << 32) | htonl(x >> 32))
+#  define ntohll(x)   ((((uint64_t)ntohl(x)) << 32) | ntohl(x >> 32))
+# endif
+#endif
+
+#define MAX(x, y) ((x) > (y)?(x):(y))
+#define MAX4(a, b, c, d) MAX(MAX(a,b),MAX(c,d))
+#define MAX6(a,b,c,d,e,f) MAX(MAX4(a,b,c,d),MAX(e,f))
+#define MAX8(a,b,c,d,e,f,g,h) MAX(MAX4(a,b,c,d),MAX4(e,f,g,h))
+
+#define MIN(x,y) ((x) < (y)?(x):(y))
+#define MIN4(a, b, c, d) MIN(MIN(a,b),MIN(c,d))
+#define MIN6(a,b,c,d,e,f) MIN(MIN4(a,b,c,d),MIN(e,f))
+#define MIN8(a,b,c,d,e,f,g,h) MIN(MIN4(a,b,c,d),MIN4(e,f,g,h))
+
+#define EXPORT(sym) __attribute__((externally_visible)) sym
+#define likely(x)   __builtin_expect(x,1)
+#define unlikely(x) __builtin_expect(x,0)
+#define must_check  __attribute__((warn_unused_result))
+#define unused      __attribute__((unused))
+
+#endif
