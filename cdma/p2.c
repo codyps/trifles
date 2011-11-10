9924786372580cee9e1d091a7dcec259bdddb68a
Cody Schafer <cpschafer@gmail.com>
2011-11-10 08:02:03 -0500

    Tweakity tweak.

--- cdma/p2.c
+++ cdma/p2.c
@@ -7,16 +7,34 @@
  */
 #define _BSD_SOURCE /* for endian.h */
 
-/* 256 % 7 != 0, so 8 makes sense here */
+/* R: "Finally, your messages will be coded in 7-bit ASCII format (MSB first, left to right)." */
+/* 256 % 7 != 0, so 8 makes sense here despite the above statement */
 #define ACSII_CHAR_BITS 8
-#define FLIP_SHIFT_REG  0
+#define ACSII_RECV_MSB_FIRST 1
+
+/* R: "That is, using seed Sm , the first 256
+   bits (ordered LSB to MSB) output by the shift register will comprise sm1 ,
+   the next sm2 and so on."
+ *
+ * What does it mean for the bits to be orderd "[from] LSB to MSB"?
+ * Is the LSB the first emitted? Or the MSB?
+ */
+#define SHIFT_REG_LSB_EMIT_FIRST 1
+
+/* This should do the same thing as changing the above */
+#define FLIP_RK 0
+
+/* R: During bit interval k, user m has codeword smk composed of ±1s.
+ *
+ * binary = {1, 0} instead. */
 #define CODEWORD_IS_BINARY 0
-#define FLIP_RK         1
 
-#define RAND_INIT_FLIP  1
-#define RAND_INIT_SHIFT 2
+#define RAND_INIT_FLIP  0
+#define RAND_INIT_BYTESWAP 0
+#define RAND_INIT_SHIFT 0
 
-#define ACSII_FLIP      1
+/* Enable the use of discrete stepping */
+#define STEP_BY_256 1
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -24,9 +42,7 @@
 #include <stdlib.h>
 
 #include <arpa/inet.h> /* htonl */
-
 #include <endian.h> /* htole32, be32toh */
-
 #define swap32(x) htole32(be32toh(x))
 
 #define __unused __attribute__((__unused__))
@@ -34,6 +50,22 @@
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 #define BIT_N(A, N) (((A) & (1ull << (N))) >> (N))
 #define MASK_N(N)   ((1ull << ((unsigned long long)(N))) - 1)
+
+/* from http://graphics.stanford.edu/~seander/bithacks.html */
+__unused static uint32_t flip_bits32(uint32_t v)
+{
+	// swap odd and even bits
+	v = ((v >> 1) & 0x55555555) | ((v & 0x55555555) << 1);
+	// swap consecutive pairs
+	v = ((v >> 2) & 0x33333333) | ((v & 0x33333333) << 2);
+	// swap nibbles ... 
+	v = ((v >> 4) & 0x0F0F0F0F) | ((v & 0x0F0F0F0F) << 4);
+	// swap bytes
+	v = ((v >> 8) & 0x00FF00FF) | ((v & 0x00FF00FF) << 8);
+	// swap 2-byte long pairs
+	v = ( v >> 16             ) | ( v               << 16);
+	return v;
+}
 
 static uint32_t update_pn(uint32_t cur)
 {
@@ -154,7 +186,7 @@ static long long array_mult_by_bit(long long *a, uint8_t *b, size_t elems)
 
 	size_t i;
 	for(i = 0; i < elems; i++) {
-#if !FLIP_SHIFT_REG
+#if SHIFT_REG_LSB_EMIT_FIRST
 		size_t bit_ix = i;
 #else
 		size_t bit_ix = elems - i - 1;
@@ -167,10 +199,10 @@ static long long array_mult_by_bit(long long *a, uint8_t *b, size_t elems)
 #endif
 
 		size_t r_ix = i;
-		long long r_k   = a[r_ix];
 #if FLIP_RK
-		r_k = a[elems - r_ix - 1];
+		r_ix = elems - i - 1;
 #endif
+		long long r_k   = a[r_ix];
 
 		sum += s_m_k * r_k;
 	}
@@ -194,14 +226,19 @@ int main(__unused int argc, __unused char **argv)
 
 	FILE *in = fopen(argv[1], "r");
 
-	unsigned long long pi = htonl(arg_ull(argv[2]));
+	unsigned long long pi = arg_ull(argv[2]);
+#if RAND_INIT_BYTESWAP
+	pi = htonl(pi);
+#endif
+
 #if RAND_INIT_FLIP
-	pi = swap32(pi);
+	pi = flip_bits32(pi);
 #endif
 
 	/* allows correcting for the previous flip */
 	pi >>= RAND_INIT_SHIFT;
 
+	fprintf(stderr, "pi = %llx\n", 0xffffffffllu);
 	fprintf(stderr, "pi = %llx\n", pi);
 
 	struct sr sr = { pi , {0}, 0 };
@@ -210,26 +247,38 @@ int main(__unused int argc, __unused char **argv)
 
 	char b = 0;
 
-	size_t i = 0, j = 0;
+	size_t i = 0;
+#if STEP_BY_256
+	size_t j = 0;
+#endif
+
+#if !STEP_BY_256
+	step_sr(&sr);
+#endif
+
 	while(!feof(in)) {
 		/* r_k = sum(m=1, M, a_m_k * b_m_k * s_m_k)
 		 * b_m_k = 1 | r_k s_m_k > 0, otherwise -1
 		 */
 		long long r_k   = next_rk(in);
+
+#if STEP_BY_256
 		r[j] = r_k;
-		//shift_in_ll(r, ARRAY_SIZE(r), r_k);
+#else
+		shift_in_ll(r, ARRAY_SIZE(r), r_k);
+#endif
 
-
+#if STEP_BY_256
 		j++;
 		if ((j % 256) == 0) {
 			j = 0;
-
 			step_sr(&sr);
+#endif
 
 			long long b_m_k = array_mult_by_bit(r, sr.codeword, 256);
 
 			//print_array(r, ARRAY_SIZE(r), stderr);
-#if ACSII_FLIP
+#if ACSII_RECV_MSB_FIRST
 			size_t ix = ACSII_CHAR_BITS - i - 1;
 #else
 			size_t ix = i;
@@ -244,7 +293,13 @@ int main(__unused int argc, __unused char **argv)
 				b = 0;
 				i = 0;
 			}
+#if STEP_BY_256
 		}
+#endif
+
+#if !STEP_BY_256
+		update_sr(&sr);
+#endif
 
 	}
 
