f9857a339fc46001bf50b1102c4d168fe1b845e3
Cody P Schafer <jmesmon@gmail.com>
2012-10-03 16:05:28 -0700

    actuall add debugfs-test.

---
+++ debugfs_test.c
@@ -0,0 +1,85 @@
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/stat.h>
+#include <sys/inotify.h>
+
+#define DO_PATH "/sys/kernel/debug/debugfs-test/do-it"
+
+int get_attr(int fd, long long *val, int base)
+{
+	char data[24];
+	ssize_t r = pread(fd, data, sizeof(data), 0);
+	if (r < 1) {
+		*val = -1;
+		return -1;
+	}
+
+	errno = 0;
+	*val = strtoll(data, NULL, base);
+	if (errno) {
+		*val = -2;
+		return -1;
+	}
+
+	return 0;
+}
+
+int get_attr_by_path(const char *path, long long *val, int base)
+{
+	int ret = 0, fd = open(path, O_RDONLY);
+	if (fd < 0) {
+		*val = -3;
+		ret = -1;
+		goto e_close;
+	}
+
+	ret = get_attr(fd, val, base);
+
+e_close:
+	close(fd);
+	return ret;
+}
+
+void read_val(int fd)
+{
+	long long v;
+	int ret = get_attr(fd, &v, 10);
+	if (ret < 0) {
+		fprintf(stderr, "failed to get attr: %s\n", strerror(errno));
+		return;
+	}
+
+	printf("initial value = %lld\n", v);
+}
+
+int main(int argc, char **argv)
+{
+	int ifd = inotify_init();
+	if (ifd < 0) {
+		fprintf(stderr, "failed to initialize inotify\n");
+		return 1;
+	}
+
+	int ret = inotify_add_watch(ifd, DO_PATH, IN_MODIFY);
+	if (ret < 0) {
+		fprintf(stderr, "failed to add watch\n");
+		return 2;
+	}
+
+
+	int fd = open(DO_PATH, O_RDWR);
+	if (fd < 0) {
+		fprintf(stderr, "failed to open file.\n");
+		return 3;
+	}
+
+	read_val(fd);
+	read_val(fd);
+	return 0;
+}
