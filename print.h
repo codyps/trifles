f415183192dda9a555e7b6c07f7616dfd153618a
Cody P Schafer <jmesmon@gmail.com>
2013-03-14 19:04:49 -0500

    some printers.

--- print.h
+++ print.h
@@ -11,6 +11,84 @@ static inline void print_hex_byte(char byte, FILE *f)
 	putc(hex_lookup[byte & 0x0f], f);
 }
 
+/* converts non-printable & control characters to their escaped represenation,
+ * if on exsists, otherwise converts to hex */
+static inline void print_bytes_as_sh_dollar_quote_string(void *data, size_t data_len, FILE *f)
+{
+
+}
+
+static inline void print_bytes_as_sh_double_quote_string(void *data, size_t data_len, FILE *f)
+{
+	char *p = data;
+	size_t i;
+	putc('"', f);
+	for (i = 0; i < data_len; i++) {
+		char c = p[i];
+		switch (c) {
+		case '"':
+		case '\\':
+		case '$':
+		case '`':
+			putc('\\', f);
+		}
+	}
+	putc('"', f);
+}
+
+/* not good for control & nonprintable characters */
+static inline void print_bytes_as_sh_single_quote_string(void *data, size_t data_len, FILE *f)
+{
+	char *p = data;
+	size_t i;
+	putc(''', f);
+	for (i = 0; i < data_len; i++) {
+		char c = p[i];
+		if (c == ''') {
+			fputs("'\\''", f);
+		} else {
+			putc(c, f);
+		}
+	}
+	putc(''', f);
+}
+
+/* Prints a string such that it can be pasted into sh. Does not use quotes
+ * - does not work well with non-printable and control characters. */
+static inline void print_bytes_as_sh_no_quote_string(void *data, size_t data_len, FILE *f)
+{
+	char *p = data;
+	size_t i;
+	for (i = 0; i < data_len; i++) {
+		char c = p[i];
+		switch (c) {
+			/* metacharacters */
+			case '|':
+			case '&':
+			case ';':
+			case '(':
+			case ')':
+			case '<':
+			case '>':
+			case ' ':
+			case '\t':
+
+			/* history expansion */
+			case '!':
+
+			/* others */
+			case ''':
+			case '"':
+			case '`':
+			case '$':
+			case '\\':
+			case '\n':
+				putc('\\', f);
+		}
+		putc(c, f);
+	}
+}
+
 static inline void print_bytes_as_cstring(void *data, size_t data_len, FILE *f)
 {
 	putc('"', f);
