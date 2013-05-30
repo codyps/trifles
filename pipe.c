84a34104430f5fc97494047fa6e8084abbf8f638
Cody P Schafer <cody@linux.vnet.ibm.com>
2013-05-30 04:00:22 -0700

    fixup.

--- pipe.c
+++ pipe.c
@@ -1,22 +1,31 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#define CHK_NON_NEG(x) do {	\
+	typeof(x) __r = x;	\
+	if (__r < 0) {		\
+		printf(" bad value for \"%s\" :  %lld\n", #x, (long long) __r);	\
+	}			\
+} while(0)
+
+#define C CHK_NON_NEG
+
 int main(void)
 {
 	int fd[2];
-	char buf[256];
+	char buf[1];
 	pipe(fd);
 	if (fork()) {
 		puts("a");
 		puts("a ping");
-		write(fd[0], "a", 1);
-		read(fd[1], buf, 1);
-		puts("a pong, done");
+		C(write(fd[0], "a", 1));
+		C(read(fd[1], buf, 1));
+		printf("a pong (%d), done\n", buf[0]);
 	} else {
 		puts("b");
-		read(fd[1], buf, 1);
-		puts("b pong, ping");
-		write(fd[0], "b", 1);
+		C(read(fd[1], buf, 1));
+		printf("b pong (%d), ping\n", buf[0]);
+		C(write(fd[0], "b", 1));
 		puts("b done");
 	}
 
