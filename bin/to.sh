72220d55aee59ccec00b06195c5b13caaaf891e7
Cody P Schafer <jmesmon@gmail.com>
2013-07-29 16:59:14 -0700

    IDEAS.

--- bin/to.sh
+++ bin/to.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-opts="--nogit --nogit-fallback --norolestats --pattern-depth=1"
+#opts="--nogit --nogit-fallback --norolestats --pattern-depth=1"
+opts="--norolestats --pattern-depth=1"
 
-if [[ $(basename $1) =~ ^0000- ]] ; then
-    ./scripts/get_maintainer.pl --nom $opts $(dirname $1)/*
-else
-    maint=$(./scripts/get_maintainer.pl --nol $opts $1)
-
-    if [ "$maint" == "" ] ; then
-        echo "[hidden email]"
+for i in "$@"; do
+    if [[ $(basename $1) =~ ^0000- ]] ; then
+        ./scripts/get_maintainer.pl --nom $opts $(dirname $1)/*
     else
-        echo "$maint"
+        maint=$(./scripts/get_maintainer.pl --nol $opts $1)
+
+        if [ "$maint" == "" ] ; then
+            echo "[hidden email]"
+        else
+            echo "$maint"
+        fi
     fi
-fi 
+done | sort -u
