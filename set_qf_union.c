f672986d8f9a8412f32762de89795e47a041892f
Cody P Schafer <devel@codyps.com>
2013-08-26 22:32:51 -0700

    blah

--- set_qf_union.c
+++ set_qf_union.c
@@ -3,22 +3,16 @@
 #include <ctype.h>
 
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof((x)[0]))
-#define field_size(t,f) sizeof(((t *)NULL)->f)
 
 struct set {
 	int id[10];
-	int sz[10];
 };
-
-_Static_assert(field_size(struct set,id) == field_size(struct set,sz),
-		"id and sz must be the same length");
 
 static void set_init(struct set *s)
 {
 	size_t i;
 	for (i = 0; i < ARRAY_SIZE(s->id); i++) {
 		s->id[i] = (int)i;
-		s->sz[i] = 1;
 	}
 }
 
@@ -27,7 +21,6 @@ static int set_root(struct set *s, int elem)
 	while (s->id[elem] != elem) {
 		elem = s->id[elem];
 	}
-
 	return elem;
 }
 
@@ -36,12 +29,10 @@ static void set_union(struct set *s, int p, int q)
 	p = set_root(s, p);
 	q = set_root(s, q);
 
-	if (s->sz[p] >= s->sz[q]) {
-		s->id[q] = p;
-		s->sz[q] += s->sz[p];
-	} else {
-		s->id[p] = q;
-		s->sz[p] += s->sz[q];
+	size_t i;
+	for (i = 0; i < ARRAY_SIZE(s->id); i++) {
+		if (s->id[i] == p)
+			s->id[i] = q;
 	}
 }
 
@@ -62,6 +53,8 @@ int main(int argc, char **argv)
 	struct set s;
 	set_init(&s);
 	char *u = strtok(argv[1], " ");
+	printf("     ");
+	set_print(&s, stdout);
 
 	while (u) {
 		if (strlen(u) != 3)
@@ -78,9 +71,13 @@ int main(int argc, char **argv)
 
 		set_union(&s, u[0] - '0', u[2] - '0');
 
+		printf("%s: ", u);
+		set_print(&s, stdout);
 		u = strtok(NULL, " ");
 	}
 
+	printf("\n");
 	set_print(&s, stdout);
+
 	return 0;
 }
