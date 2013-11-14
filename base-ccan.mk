fc56a98fa4cdf4cfe84821a5ddd04173ddd0432c
Cody P Schafer <devel@codyps.com>
2013-11-13 19:09:28 -0800

    hack for ld

--- base-ccan.mk
+++ base-ccan.mk
@@ -1,8 +1,14 @@
 CCAN_CFLAGS ?= $(C_CFLAGS)
 
+ifeq ($(findstring clang,$(CC)),)
+CCAN_LD ?= ld
+else
+CCAN_LD ?= llvm-link
+endif
+
 ccan: FORCE
 	$(MAKE) $(MAKE_ENV) CCAN_CFLAGS="$(CCAN_CFLAGS)" CCAN_LDFLAGS="$(CCAN_LDFLAGS)" \
-		LD="ld" --no-print-directory -C ccan $(MAKEFLAGS)
+		LD="$(CCAN_LD)" --no-print-directory -C ccan $(MAKEFLAGS)
 dirclean: clean
 	$(MAKE) $(MAKE_ENV) --no-print-directory -C ccan $(MAKEFLAGS) clean
 
