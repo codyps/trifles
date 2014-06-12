de2600f75d7a6b12880751ddffa8d979f71eb8a9
Cody P Schafer <devel@codyps.com>
2014-06-12 18:46:57 -0500

    type_info: reorg

--- type_info.h
+++ type_info.h
@@ -10,14 +10,15 @@
 		((1ull << ((bits) - 1) << 1) - 1)
 
 #define byte_width_max(bytes) bit_width_max((bytes) * 8)
-#define max_of_u(t) byte_width_max(sizeof(t))
-
-/* assumes 2s compliment form */
-#define max_of_s(t) max_of_2c(t)
 
 #define byte_width_min_2c(bytes) -bit_width_min_2c_pos(bytes)
 #define byte_width_min_2c_pos(bytes) bit_width_max((bytes) * 8)
 #define byte_width_max_2c(bytes) bit_width_max((bytes) * 8 - 1)
+
 #define max_of_2c(t) byte_width_max_2c(sizeof(t))
+#define max_of_u(t) byte_width_max(sizeof(t))
+
+/* assumes 2s compliment form */
+#define max_of_s(t) max_of_2c(t)
 
 #endif
