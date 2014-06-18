6441ed285d6dd6518ca825bfd496c2d60d0a7532
Cody P Schafer <dev@codyps.com>
2014-06-17 22:40:53 -0400

    test framework

---
+++ test.h
@@ -0,0 +1,34 @@
+#ifndef PENNY_TEST_H_
+#define PENNY_TEST_H_
+
+# if TEST
+static unsigned long test__error_ct = 0;
+#  define test_eq_xp(a, b) test_eq_fmt_exp((a), (b), LLU_FMT, LLU_PAIR_EXP, LLU_EQ)
+#  define test_eq_x(a, b) test_eq_fmt_exp((a), (b), "0x%llx", LLU, EQ)
+#  define test_eq_fmt(a, b, fmt) test_eq_fmt_exp(a, b, fmt, UNIT, EQ)
+#  define test_eq_fmt_exp(a, b, fmt, exp, eq) do {			\
+	typeof(a) __test_eq_a = (a);						\
+	typeof(b) __test_eq_b = (b);						\
+	if (!eq((__test_eq_a), (__test_eq_b))) {				\
+		fprintf(stderr, "TEST FAILURE: %s ("fmt") != %s ("fmt")\n",	\
+				#a, exp(__test_eq_a), #b, exp(__test_eq_b));	\
+		test__error_ct ++;						\
+	}									\
+} while(0)
+#  define test_done() do {							\
+	if (test__error_ct > 0)	{						\
+		fprintf(stderr, "TESTS FAILED: %lu, exiting\n", test__error_ct);	\
+		exit(1);							\
+	}									\
+} while(0)
+# else /* if !TEST */
+#  define test_eq_x(a, b) BUILD_BUG_ON_INVALID((a) == (b))
+#  define test_eq_fmt(a, a_fmt, b, b_fmt) do {	\
+	BUILD_BUG_ON_INVALID((a) == (b));	\
+	printf_check_fmt(a_fmt b_fmt, a, b);	\
+} while(0)
+#  define test(x) BUILD_BUG_ON_INVALID(e)
+#  define test_done() do {} while(0)
+# endif /* !TEST */
+
+#endif
