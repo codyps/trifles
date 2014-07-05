9b127f6c328ffe8e243eeb0a825ec7193a43dd73
Cody P Schafer <dev@codyps.com>
2014-07-05 09:54:27 -0400

    forward

--- comp_mask.c
+++ comp_mask.c
@@ -9,8 +9,15 @@
 #define TEST 1
 #include <penny/test.h>
 
-#define bit_width_max(bits) \
-		((1ull << ((bits) - 1) << 1) - 1)
+/* assert(bits < (sizeof(1ull) * CHAR_BIT)) */
+#define bit_mask_nf(bits) ((1ull << (bits)) - 1)
+/* assert(bits > 0) */
+#define bit_mask_nz(bits) ((1ull << ((bits) - 1) << 1) - 1)
+
+#define bit_mask(bits) ({			\
+	__typeof__(bits) __bm_bits = (bits);	\
+	__bm_bits ? bit_mask_nz(__bm_bits) : 0;	\
+})
 
 #define ROUND_UP_POW_OF_2X_M1(_val, _x)   ((_val) | bit_width_max(_x))
 #define ROUND_DOWN_POW_OF_2X(_val, _x) (((_val) >> (_x)) << (_x))
@@ -58,7 +65,9 @@ static unsigned fls_32(uint32_t v)
 
 static unsigned ilog_32(uint32_t v)
 {
-	return fls_32(v);
+	if (!v)
+		return 0;
+	return fls_32(v) - 1;
 }
 
 #define ALIGN_OF(x) ctz_32(x)
@@ -88,8 +97,16 @@ static struct base_mask match_range_fix_low(uint32_t edge, uint32_t max)
 #else
 static struct base_mask match_range_fix_low(uint32_t base, uint32_t max)
 {
-	printf(">> %jx %jx\n", (uintmax_t)ctz_32(base), (uintmax_t)((1 << ctz_32(base)) - 1));
-	return (struct base_mask){ base, (1 << ilog_32(((1 << ctz_32(base)) - 1) & max)) - 1 };
+	unsigned base_tz = ctz_32(base);
+	uint32_t mask_1 = bit_mask(base_tz);
+	uint32_t masked_max = max & mask_1;
+	unsigned log_of_max_masked = ilog_32(masked_max);
+	uint32_t final_mask = bit_mask(log_of_max_masked);
+
+	printf("base %04jx max %04jx base_tz %04x mask_1 %04jx masked_max %04jx %04x %04jx\n",
+			(uintmax_t)base, (uintmax_t)max, base_tz, (uintmax_t)mask_1, (uintmax_t)masked_max, log_of_max_masked, (uintmax_t)final_mask);
+
+	return (struct base_mask){ base, final_mask };
 }
 #endif
 
@@ -134,8 +151,10 @@ int main(void)
 	ok1(!matches(match_range_fix_low(0xfff0, 0xfff1), 0xfff2));
 	ok1(!matches(match_range_fix_low(0xfff1, 0xfff1), 0xfff2));
 	ok1(!matches(match_range_fix_low(0xfff1, 0xfff1), 0xfff0));
-	test_bm(BM(0xffe0, 0xf),  match_range_fix_low(0xffe0, 0xfff0));
+
+	test_bm(BM(0xffe0, 0x0f),  match_range_fix_low(0xffe0, 0xfff0));
 	ok_eq(matcher_max(match_range_fix_low(0xffe0, 0xfff1)), (uint32_t)0xfff1);
+
 	test_bm(BM(0xffe0, 0x1f), match_range_fix_low(0xffe0, 0xffff));
 	test_bm(BM(0xffe0, 0x1),  match_range_fix_low(0xffe0, 0xffe1));
 	test_done();
