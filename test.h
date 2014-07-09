398622e1f5a703581949450462f6f97fe819c53e
Cody P Schafer <cschafer@cybexintl.com>
2014-07-09 10:55:01 -0400

    penny test rework

--- test.h
+++ test.h
@@ -3,64 +3,21 @@
 
 #include <stdlib.h>
 
-# if TEST
-#  ifndef TEST_FILE
-#   define TEST_FILE stderr
-#  endif
+#ifndef TEST_FILE
+# define TEST_FILE stderr
+#endif
 static unsigned long test__error_ct = 0;
-#  define NUM_FMT "%#jx"
-#  define NUM_EXP(a) ((uintmax_t)(a))
-#  define NUM_EQ(a, b) ((uintmax_t)(a) == (uintmax_t)(b))
-#  define ok_eq(a, b) test_eq_fmt_exp((a), (b), NUM_FMT, NUM_EXP, NUM_EQ)
-#  define test_eq_xp(a, b) test_eq_fmt_exp((a), (b), LLU_FMT, LLU_PAIR_EXP, LLU_EQ)
-#  define test_eq_x(a, b) test_eq_fmt_exp((a), (b), "0x%llx", LLU, EQ)
-#  define test_eq_fmt(a, b, fmt) test_eq_fmt_exp(a, b, fmt, UNIT, EQ)
-#  define test_eq_fmt_exp(a, b, fmt, exp, eq) do {			\
-	typeof(a) __test_eq_a = (a);						\
-	typeof(b) __test_eq_b = (b);						\
-	if (!eq((__test_eq_a), (__test_eq_b))) {				\
-		fprintf(TEST_FILE, "%s:%d - TEST FAILURE: %s ("fmt") != %s ("fmt")\n",	\
-				__FILE__, __LINE__, #a, exp(__test_eq_a), #b, exp(__test_eq_b));	\
-		test__error_ct ++;						\
-	}									\
-} while(0)
-#  define ok_cmp_(a, cmp, b, fmt, exp) do {					\
-	typeof(a) __test_eq_a = (a);							\
-	typeof(b) __test_eq_b = (b);							\
-	if (!((__test_eq_a) cmp (__test_eq_b))) {							\
-		fprintf(TEST_FILE, __FILE__ ":%d - TEST FAILURE: " #a " ("fmt") !" #cmp " " #b " ("fmt")\n",	\
-				__LINE__, exp(__test_eq_a), exp(__test_eq_b));	\
-		test__error_ct ++;						\
-	}									\
+
+#define ok_(a, file, line, ...) do {							\
+	if (!(a)) {									\
+		fprintf(TEST_FILE, "[x] Failure at " file ":" #line" - " __VA_ARGS__);	\
+		test__error_ct++;							\
+	}										\
 } while (0)
-#  define ok_cmp(a, cmp, b) ok_cmp_(a, cmp, b, NUM_FMT, NUM_EXP)
-#  define ok1(a) do {						\
-	if (!(a)) {						\
-		fprintf(TEST_FILE, "%s:%d - TEST FAILURE: %s\n", __FILE__, __LINE__, #a);	\
-		test__error_ct++;				\
-	}							\
-} while(0)
+#define ok(a, ...) ok_(a, __FILE__, __LINE__, __VA_ARGS__)
+#define ok1(a) ok(a, #a)
 
+#define exit_status() test__error_ct
 
-
-#  define test_done() do {							\
-	if (test__error_ct > 0)	{						\
-		fprintf(TEST_FILE, "TESTS FAILED: %lu, exiting\n", test__error_ct);	\
-		exit(1);							\
-	}									\
-} while(0)
-# else /* if !TEST */
-#  define ok_eq(a, b) do {		\
-	BUILD_BUG_ON_INVALID(a);	\
-	BUILD_BUG_ON_INVALID(b);	\
-} while (0)
-#  define test_eq_x(a, b) BUILD_BUG_ON_INVALID((a) == (b))
-#  define test_eq_fmt(a, a_fmt, b, b_fmt) do {	\
-	BUILD_BUG_ON_INVALID((a) == (b));	\
-	printf_check_fmt(a_fmt b_fmt, a, b);	\
-} while(0)
-#  define ok1(x) BUILD_BUG_ON_INVALID(e)
-#  define test_done() do {} while(0)
-# endif /* !TEST */
 
 #endif
