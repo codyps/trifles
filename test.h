6244c013f82835d2f4b4f98711cbe84962d4d19e
Cody P Schafer <dev@codyps.com>
2014-12-24 17:09:58 -0500

    test extentions

--- test.h
+++ test.h
@@ -8,9 +8,9 @@
 #endif
 static unsigned long test__error_ct = 0;
 
-#define ok_(a, file, line, ...) do {							\
-	if (!(a)) {									\
-		fprintf(TEST_FILE, "[x] Failure at " file ":" #line" - " __VA_ARGS__);	\
+#define ok_(a, file, line, fmt, ...) do {							 \
+	if (!(a)) {										 \
+		fprintf(TEST_FILE, "[x] Failure at " file ":" #line" - " fmt "\n", ## __VA_ARGS__); \
 		test__error_ct++;							\
 	}										\
 } while (0)
@@ -19,5 +19,17 @@ static unsigned long test__error_ct = 0;
 
 #define exit_status() test__error_ct
 
+
+/* Extentions */
+#define test_eq_x(a, b) ok((a) == (b), "%llu != %llu", (long long unsigned)a, (long long unsigned)b)
+#define test_eq_xp(_a, _b) ok((_a).a == (_b).a && (_a).b == (_b).a, "%llu, %llu != %llu, %llu", \
+		(_a).a, (_a).b, (_b).a, (_b).b)
+
+#define test_done() do {						\
+	if (test__error_ct > 0) {					\
+		fprintf(TEST_FILE, "[!] Failed %lu times, exiting\n", test__error_ct);	\
+		exit(exit_status());					\
+	}								\
+} while (0)
 
 #endif
