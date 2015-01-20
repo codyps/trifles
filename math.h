bc287c96e726b28887305fa4a9cd83fee38561e8
Cody P Schafer <dev@codyps.com>
2015-01-19 21:57:18 -0500

    DIV_ROUND_CLOSEST

--- math.h
+++ math.h
@@ -24,6 +24,22 @@
 #define DIV_ROUND_UP_POS_OR_ZERO(n,d)
 #endif
 
+/*
+ * Divide positive or negative dividend by positive divisor and round
+ * to closest integer. Result is undefined for negative divisors and
+ * for negative dividends if the divisor variable type is unsigned.
+ */
+#define DIV_ROUND_CLOSEST(x, divisor)(			\
+{							\
+	typeof(x) __x = x;				\
+	typeof(divisor) __d = divisor;			\
+	(((typeof(x))-1) > 0 ||				\
+	 ((typeof(divisor))-1) > 0 || (__x) > 0) ?	\
+		(((__x) + ((__d) / 2)) / (__d)) :	\
+		(((__x) - ((__d) / 2)) / (__d));	\
+}							\
+)
+
 static inline uint16_t linear_interp_u16(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x)
 {
 	/* potential for multiplication overflow */
