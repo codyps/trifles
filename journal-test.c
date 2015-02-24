0f4b5de9edc4a53f8a5b9df3b090d3ae6b73a2c0
Cody P Schafer <dev@codyps.com>
2015-02-24 11:11:46 -0500

    things

---
+++ journal-test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	puts("<0>This is 0");
+	puts("<1>This is 1");
+	puts("<2>This is 2");
+	puts("<3>This is 3");
+	puts("<9>This is 9");
+	puts("This is nothing");
+	puts("<8>This is 8");
+	return 0;
+}
