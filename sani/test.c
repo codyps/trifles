e9eaa64fd1323c4ac2290c36c4bbd965e3512e65
Cody P Schafer <dev@codyps.com>
2015-12-23 22:12:23 -0500

    try to test things a bit more

--- sani/test.c
+++ sani/test.c
@@ -6,5 +6,9 @@ int main(int argc, char **argv)
 	(void)argv;
 	int x = argc + 1 + INT_MAX;
 	intmax_t v = (argc + 1) * INTMAX_MAX;
-	return x | v;
+	x = INT_MIN;
+	x -= 2 + argc;
+	int8_t y = INT8_MIN;
+	y -= 2 + argc;
+	return x | v | y;
 }
