fa2b2f9b7fd692147e03c95361c2cbc82c0bc8ed
Cody P Schafer <dev@codyps.com>
2018-11-04 20:57:30 -0500

    slight variation

--- pr_generic.c
+++ pr_generic.c
@@ -33,21 +33,23 @@ static inline void pr__charp(const char *p)
 #endif
 
 // apply on each arg structure (for up to 5 args)
-#define invoke_0(m, ...)
-#define invoke_1(m, a) m(a)
-#define invoke_2(m, a, ...) m(a) invoke_1(m, __VA_ARGS__)
-#define invoke_3(m, a, ...) m(a) invoke_2(m, __VA_ARGS__)
-#define invoke_4(m, a, ...) m(a) invoke_3(m, __VA_ARGS__)
-#define invoke_5(m, a, ...) m(a) invoke_4(m, __VA_ARGS__)
+#define map_0(m, ...)
+#define map_1(m, a) m(a)
+#define map_2(m, a, ...) m(a) map_1(m, __VA_ARGS__)
+#define map_3(m, a, ...) m(a) map_2(m, __VA_ARGS__)
+#define map_4(m, a, ...) m(a) map_3(m, __VA_ARGS__)
+#define map_5(m, a, ...) m(a) map_4(m, __VA_ARGS__)
+#define MAP__(n, m, ...) map_##n(m, __VA_ARGS__)
+#define MAP_(...) MAP__(__VA_ARGS__)
 
-#define arg_n5(_1,_2,_3,_4,_5,N,...) N
+#define ARG(_1,_2,_3,_4,_5,N,...) N
+#define ARGN(...) ARG(__VA_ARGS__,5,4,3,2,1,0)
 
-#define for_each_apply(m, ...) \
-	arg_n5(__VA_ARGS__, invoke_5, invoke_4, invoke_3, invoke_2, invoke_1, invoke_0)(m, __VA_ARGS__)
+#define MAP(m, ...) MAP_(ARGN(__VA_ARGS__),m,  __VA_ARGS__)
 
 #define pr_g_(x) pr_g(x);
 #define pr(...) do { \
-	for_each_apply(pr_g_, __VA_ARGS__); \
+	MAP(pr_g_, __VA_ARGS__); \
 } while (0)
 
 int main(void)
