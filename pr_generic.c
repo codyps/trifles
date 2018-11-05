5dabcbc1bab1828666c12e9d3a74b2e7e4cdbc26
Cody P Schafer <dev@codyps.com>
2018-11-04 20:40:47 -0500

    5 args only :(

--- pr_generic.c
+++ pr_generic.c
@@ -26,16 +26,35 @@ static inline void pr__charp(const char *p)
     char*: pr__charp(x) \
 )
 #else
-#define PR_G(x) _Generic((x), \
+#define pr_g(x) _Generic((x), \
     int: pr__int, \
     char*: pr__charp \
 )(x)
 #endif
 
+// apply on each arg structure (for up to 5 args)
+#define invoke_0(m, ...)
+#define invoke_1(m, a) m(a)
+#define invoke_2(m, a, ...) m(a) invoke_1(m, __VA_ARGS__)
+#define invoke_3(m, a, ...) m(a) invoke_2(m, __VA_ARGS__)
+#define invoke_4(m, a, ...) m(a) invoke_3(m, __VA_ARGS__)
+#define invoke_5(m, a, ...) m(a) invoke_4(m, __VA_ARGS__)
+
+#define arg_n5(_1,_2,_3,_4,_5,N,...) N
+
+#define for_each_apply(m, ...) \
+	arg_n5(__VA_ARGS__, invoke_5, invoke_4, invoke_3, invoke_2, invoke_1, invoke_0)(m, __VA_ARGS__)
+
+#define pr_g_(x) pr_g(x);
+#define pr(...) do { \
+	for_each_apply(pr_g_, __VA_ARGS__); \
+} while (0)
+
 int main(void)
 {
-    PR_G("hi ");
-    PR_G(1);
+    pr_g("hi ");
+    pr_g(1);
+    pr(" bye ", 2);
     putchar('\n');
     return 0;
 }
