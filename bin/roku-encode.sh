17d7c284455dfd842ec950333c9483ce8247249b
Cody Schafer <dev@codyps.com>
2018-11-21 18:26:42 -0500

    dos2unix

--- bin/roku-encode.sh
+++ bin/roku-encode.sh
@@ -3,6 +3,11 @@
 set -euf -o pipefail
 
 inf="$1"
-outf="$(dirname "$1")/roku.$(basename "$1")"
+
+if [ $# -eq 1 ]; then
+	outf="$(dirname "$1")/roku.$(basename "$1")"
+else
+	outf="$2"
+fi
 
 ffmpeg -i "$1" -map 0:v -c:v copy -map 0:a -c:a:0 copy -map 0:a -c:a:1 flac "$outf"
