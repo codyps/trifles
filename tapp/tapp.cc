3d258e08d4e0a2886a684257a02eb56950b9336e
Cody P Schafer <dev@codyps.com>
2020-04-24 16:29:56 -0400

    tapp(unsure what this is for)

---
+++ tapp/tapp.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+struct printer {
+	printer(boost::asio::io_context& io)
+		: timer_(io, boost::posix_time::seconds(1))
+		, count_(0)
+	{
+		timer_.async_wait(boost::bind(&printer::print, this));
+	}
+
+	~printer()
+	{
+		std::cout << "final: " << count_ << std::endl;
+	}
+
+	void print()
+	{
+		if (count_ < 5) {
+			std::cout << count_ << std::endl;
+			++count_;
+
+			timer_.expires_at(timer_.expires_at() + boost::posix_time::seconds(1));
+			timer_.async_wait(boost::bind(&printer::print, this));
+		}
+	}
+
+private:
+	boost::asio::deadline_timer timer_;
+	int count_;
+};
+
+
+int main()
+{
+	boost::asio::io_service io;
+	printer p(io);
+	io.run();
+	return 0;
+}
