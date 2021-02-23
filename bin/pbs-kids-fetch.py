9d8f28a209fbcae572e52da7cc412d4c44ef9ca2
Cody Schafer <dev@codyps.com>
2021-02-23 11:19:41 -0500

    ffmpeg path control

--- bin/pbs-kids-fetch.py
+++ bin/pbs-kids-fetch.py
@@ -10,6 +10,7 @@ parser.add_argument('url', type=str,
                     help='A pbs kids show-list url to fetch shows from', nargs=1)
 parser.add_argument('--video-profile', type=str, help='profile of the video stream to download', default='hls-1080p-16x9')
 parser.add_argument('--dry-run', action='store_const', const=True, help="fetch info, but don't download videos", default=False)
+parser.add_argument('--ffmpeg-path', help="use this executable as ffmpeg", default='ffmpeg')
 
 def main():
     
@@ -18,6 +19,7 @@ def main():
     url = args.url[0]
     pick_profile = args.video_profile
     dry_run = args.dry_run
+    ffmpeg = args.ffmpeg_path
     #url = 'https://producerplayer.services.pbskids.org/show-list/?shows=odd-tube&shows_title=Odd%20Tube&page=1&page_size=20&available=public&sort=-encored_on'
 
     r = requests.get(url)
@@ -53,7 +55,7 @@ def main():
         if vid_url is None:
             print(f"could not find video for profile {profile}")
 
-        cmd = ["ffmpeg", "-f", "hls", "-i", vid_url, "-c", "copy", f"{name}.mkv"]
+        cmd = [ffmpeg_path, "-f", "hls", "-i", vid_url, "-c", "copy", f"{name}.mkv"]
 
         if dry_run:
             print(f"would run: {cmd}")
