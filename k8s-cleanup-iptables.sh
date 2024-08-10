835ac9ad92093ecb08a3cd6daf3a98cbe71777f8
Cody P Schafer <dev@codyps.com>
2024-08-10 17:17:22 -0400

    k8s-cleanup-iptables: fix, use awk

--- k8s-cleanup-iptables.sh
+++ k8s-cleanup-iptables.sh
@@ -1,35 +1,61 @@
 #! /usr/bin/env bash
 set -euf -o pipefail
-set -x
 
 echo "# table: nat"
 for chain in PREROUTING INPUT OUTPUT POSTROUTING; do
    echo "# chain: $chain"
    # scan for rules pointing to tables with "CILIUM" or "KUBE" in the name
    dct=0
-   iptables -t nat -L "$chain" -n --line-numbers | grep -E 'CILIUM|KUBE' | awk '{print $1}' | while read -r line; do
-      # delete the rule
-      dl=$((line - dct))
-      echo iptables -t nat -D "$chain" $dl
-      dct=$((dct + 1))
-   done
+   iptables -t nat -L "$chain" -n --line-numbers | awk '
+   	BEGIN { dct=0 }
+	/CILIUM|KUBE/ {
+		dl=$1-dct
+		print "removing: " $0
+		cmd="iptables -t nat -D '"$chain"' " dl
+		print "running: " cmd
+		system(cmd)
+		dct++
+	}
+	'
 done
+
+# Delete chains with CILIUM or KUBE in their name
+iptables -t nat -L -n | awk '
+	/Chain (CILIUM|KUBE)/ {
+		cmd1="iptables -t nat -F " $2
+		cmd2="iptables -t nat -X " $2
+		print "running: " cmd1
+		system(cmd1)
+		print "running: " cmd2
+		system(cmd2)
+	}
+	'
 
 echo "# table: filter"
 for chain in INPUT FORWARD OUTPUT; do
    echo "# chain: $chain"
    # scan for rules pointing to tables with "CILIUM" or "KUBE" in the name
-   dct=0
-   iptables -L "$chain" -n --line-numbers | grep -E 'CILIUM|KUBE' | awk '{print $1}' | while read -r line; do
-      # delete the rule
-      dl=$((line - dct))
-      echo iptables -D "$chain" $dl
-      dct=$((dct + 1))
-   done
+   iptables -L "$chain" -n --line-numbers | awk '
+   	BEGIN { dct=0 }
+	/CILIUM|KUBE/ {
+		dl=$1-dct
+		print "removing: " $0
+		cmd="iptables -D '"$chain"' " dl
+		print "running: " cmd
+		system(cmd)
+		dct++
+	}
+	'
 done
 
-# Flush all chains with CILIUM or KUBE in their name
-echo iptables -t nat -F $(iptables -t nat -L -n | grep -E 'CILIUM|KUBE' | awk '{print $2}')
-echo iptables -F $(iptables -L -n | grep -E 'CILIUM|KUBE' | awk '{print $2}')
-echo iptables -t nat -X $(iptables -t nat -L -n | grep -E 'CILIUM|KUBE' | awk '{print $2}')
-echo iptables -X $(iptables -L -n | grep -E 'CILIUM|KUBE' | awk '{print $2}')
+# Delete chains with CILIUM or KUBE in their name
+iptables -L -n | awk '
+	/Chain (CILIUM|KUBE)/ {
+		cmd1="iptables -F " $2
+		cmd2="iptables -X " $2
+		print "running: " cmd1
+		system(cmd1)
+		print "running: " cmd2
+		system(cmd2)
+	}
+	'
